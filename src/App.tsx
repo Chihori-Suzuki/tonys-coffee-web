2eb67117d85624e9f200f1c3f22ec85c03a28605
Chihori-Suzuki <69213233+Chihori-Suzuki@users.noreply.github.com>
2021-12-12 16:38:44 -0800

    fix pics

--- src/App.tsx
+++ src/App.tsx
@@ -192,15 +192,15 @@ const Gallery = () => {
         }}
       ></div>
       <div className="container-gallery-pics">
-        <img className="gallery-pics" src="./pictures/gallery/IMG_4440.jpg" width="100%" height="100%" />
-        <img className="gallery-pics" src="./pictures/gallery/IMG_4445.jpg" width="100%" height="100%" />
-        <img className="gallery-pics" src="./pictures/gallery/IMG_4452.jpg" width="100%" height="100%" />
-        <img className="gallery-pics" src="./pictures/gallery/IMG_4457.jpg" width="100%" height="100%" />
-        <img className="gallery-pics" src="./pictures/gallery/IMG_4482.jpg" width="100%" height="100%" />
-        <img className="gallery-pics" src="./pictures/gallery/IMG_4485.jpg" width="100%" height="100%" />
-        <img className="gallery-pics" src="./pictures/gallery/IMG_4487.jpg" width="100%" height="100%" />
-        <img className="gallery-pics" src="./pictures/gallery/IMG_4489.jpg" width="100%" height="100%" />
-        <img className="gallery-pics" src="./pictures/gallery/IMG_4541.jpg" width="100%" height="100%" />
+        <img src="./pictures/gallery/IMG_4440.jpg" width="100%" height="auto" />
+        <img src="./pictures/gallery/IMG_4445.jpg" width="100%" height="auto" />
+        <img src="./pictures/gallery/IMG_4452.jpg" width="100%" height="auto" />
+        <img src="./pictures/gallery/IMG_4457.jpg" width="100%" height="auto" />
+        <img src="./pictures/gallery/IMG_4482.jpg" width="100%" height="auto" />
+        <img src="./pictures/gallery/IMG_4485.jpg" width="100%" height="auto" />
+        <img src="./pictures/gallery/IMG_4487.jpg" width="100%" height="auto" />
+        <img src="./pictures/gallery/IMG_4489.jpg" width="100%" height="auto" />
+        <img src="./pictures/gallery/IMG_4541.jpg" width="100%" height="auto" />
       </div>
     </>
   );
